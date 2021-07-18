2cf05e5102ae9c7bc457eb19e742aa0e3e06e719
Mesterduc <hongduccai@gmail.com>
2021-07-18 17:32:38 +0200

    Initial Commit

---
+++ cocktails/cocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocktailsApp.swift
+//  cocktails
+//
+//  Created by Duc hong cai on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct cocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
